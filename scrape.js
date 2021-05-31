37d2134fd30e53b80f0943ec3f8ff2a8661a5e08
Andrew Sullivan <andrew@sllvn.me>
2021-05-30 22:04:36 -0700

    Initial commit

---
+++ scrape.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const fetch = require('isomorphic-fetch')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+
+async function main () {
+  const response = await fetch('https://www.co.thurston.wa.us/sheriff/incidents-report.htm')
+  const body = await response.text()
+  // const body = fs.readFileSync('./sample.html', 'utf8')
+
+  const dom = new JSDOM(body)
+  const rows = [...dom.window.document.querySelectorAll('#center_col table tr')]
+  const data = rows.reduce((acc, row) => {
+      const tds = row.querySelectorAll('td')
+      return [
+        ...acc,
+        {
+          date: tds[0].textContent,
+          time: tds[1]?.textContent,
+          sequenceNum: tds[2]?.textContent,
+          incidentNature: tds[3]?.textContent,
+          location: tds[4]?.textContent,
+        }
+      ]
+    }, [])
+    .filter(r => !!r.time) // exclude empty rows
+    .slice(1) // exclude header row
+
+  console.log(JSON.stringify(data, null, 4))
+}
+main()
